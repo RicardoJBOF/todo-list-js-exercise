1ef6acfc67d64be8b6651accec1e9ffc3be1e2be
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-24 10:59:41 -0500

    Refactoring index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-
+// FUNCTION IMPLEMENTATION
 function newTask(title, description) {
   const task = {
-    title: title,
-    description: description,
+    title,
+    description,
     complete: false,
 
-    markCompleted: function() {
+    markCompleted: function () {
       this.complete = true;
     },
 
-    logState: function() {
-      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
-    }
+    logState: function () {
+      console.log(
+        `${this.title} has${this.complete ? " " : " not "}been completed`
+      );
+    },
   };
   return task;
 }
 
-
-
-
 // DRIVER CODE BELOW
-
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task1 = newTask(
+  "Clean Cat Litter",
+  "Take all the 💩 out of the litter box"
+);
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
-task1.logState();// Clean Cat Litter has been completed
-
-
+task1.logState(); // Clean Cat Litter has been completed
